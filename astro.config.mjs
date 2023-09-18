aa3ab88fb8bfe2a5585d8e06564bdabd9df8c34a
allends <allen.davisswing@rutgers.edu>
2023-09-17 22:08:30 -0400

    updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/static';
 
-import vercel from "@astrojs/vercel/serverless";
+// import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: vercel()
+  output: 'static',
+  adapter: vercel({
+    imageService: true,
+  }),
 });
